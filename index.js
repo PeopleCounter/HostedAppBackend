ddb46cb01b922fd8fbbdf9532b7c89ceaf403475
Tejas Pundlik <tejaspundlik2@gmail.com>
2023-08-13 21:14:34 +0530

    Commit after Implementing PASSKEY

--- index.js
+++ index.js
@@ -10,37 +10,29 @@ app.listen(PORT, () => { console.log(`Connected At Port ${PORT}`) })
 app.use(cors({ origin: '*' }))
 app.use(express.json())
 
-console.log(process.env.DATABASE)
-console.log(process.env.ACCESSKEY)
-
 app.post('/guest', async (req, res) => {
-    console.log(req.body)
-    console.log(req.body.passkey)
     if (req.body.passkey == process.env.ACCESSKEY) {
-        console.log("ACCESSKEY")
-    }
-    if (req.body.passkey == process.env.ACCESS) {
-        console.log("ACCESS")
-    }
-    if (req.body.passkey == process.env.PASSKEY) {
-        console.log("PASSKEY")
-    }
-    try {
-        const data_add = new data({
-            name: req.body.name,
-            number: req.body.number,
-        })
-        console.log(req.body)
-        await data_add.save()
-        const pincode_add = new pincode({
-            pincode: req.body.pincode
-        })
-        await pincode_add.save()
+        try {
+            const data_add = new data({
+                name: req.body.name,
+                number: req.body.number,
+            })
+            console.log(req.body)
+            await data_add.save()
+            const pincode_add = new pincode({
+                pincode: req.body.pincode
+            })
+            await pincode_add.save()
+            res.status(200).send()
+        }
+        catch (err) {
+            console.log(err)
+            res.status(500).send()
+        }
         res.status(200).send()
     }
-    catch (err) {
-        console.log(err)
-        res.status(500).send()
+    else {
+        res.status(403).send()
     }
-    res.status(200).send()
+
 })
