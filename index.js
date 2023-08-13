3fb6cfbc3c6243e7201d4c7db99aa041be14fb2d
Tejas Pundlik <tejaspundlik2@gmail.com>
2023-08-13 20:31:28 +0530

    Trying to add passkey

--- index.js
+++ index.js
@@ -11,15 +11,20 @@ app.use(cors({ origin: '*' }))
 app.use(express.json())
 
 app.post('/guest', async (req, res) => {
+    console.log(req.body)
+    if (req.body.passkey != process.env.ACCESSKEY) {
+        console.log("Not Matching")
+        console.log(process.env.ACCESSKEY)
+        console.log(typeof (process.env.ACCESSKEY))
+        console.log(req.body.passkey)
+        console.log(typeof (req.body.passkey))
+    }
+    console.log(req.body.passkey == process.env.ACCESSKEY)
     try {
         const data_add = new data({
             name: req.body.name,
             number: req.body.number,
-            key: req.body.key
         })
-        if (req.body.key != process.env.ACCESS_KEY) {
-            throw 500
-        }
         console.log(req.body)
         await data_add.save()
         const pincode_add = new pincode({
